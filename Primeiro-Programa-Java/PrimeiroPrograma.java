472001c86db7f9f3a1d62ec5cba8c085a66ea211
RafaelDRicci <rafael_d_ricci@hotmail.com>
2021-10-30 15:51:16 -0300

    Criando class Livro

--- Primeiro-Programa-Java/PrimeiroPrograma.java
+++
@@ -1,8 +0,0 @@
-public class PrimeiroPrograma{
-	public static void main(String [] args){
-		System.out.println("Hello World");
-		int a = 2;
-		int b = 3;
-		System.out.println(a + " + " + b + " = " + (a + b));
-	}
-}
--- primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -5,13 +5,51 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
+        Livro livro = new Livro("Livro", 300);
         System.out.println(gato);
-
+        System.out.println(livro);
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World!");
         System.out.println(a + " + " + b + " = " + (a + b));*/
 
     }
+
 }
+    class Livro{
+
+        private String nome;
+        private Integer numeroPaginas;
+
+        public Livro() {
+        }
+
+        public Livro(String nome, Integer numeroPaginas) {
+            this.nome = nome;
+            this.numeroPaginas = numeroPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumeroPaginas() {
+            return numeroPaginas;
+        }
+
+        public void setNumeroPaginas(Integer numeroPaginas) {
+            this.numeroPaginas = numeroPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numeroPaginas=" + numeroPaginas +
+                    '}';
+        }
+    }
